c98ee090e433f34585cb98ddae1c4d175ce5fd96
Jay <jermainepaulsmith@icloud.com>
2018-07-22 21:52:06 +0100

    Initial commit adding files

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.collapsible').collapsible();
+    $('#modal1').modal();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
