167117b70b0d27f0fd781679e4345c8884c3cc66
Jay <jermainepaulsmith@icloud.com>
2018-08-08 23:10:06 +0100

    added carousel of companies

--- js/init.js
+++ js/init.js
@@ -1,9 +1,17 @@
 (function($){
   $(function(){
-
+    $('.scrollspy').scrollSpy();
+    $('.carousel').carousel({padding: 100,});
     $('.button-collapse').sideNav();
     $('.collapsible').collapsible();
     $('#modal1').modal();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// Autoplay
+autoplay()   
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
