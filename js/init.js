3d09c887c47c6bd58dbad6e190fc20aaea3c38e9
Jay <jermainepaulsmith@icloud.com>
2020-11-08 12:34:16 +0000

    updated init file to jquery

--- js/init.js
+++ js/init.js
@@ -1,31 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-  M.AutoInit();
-  // const carousel = document.querySelectorAll(".carousel");
-  // let instances = M.Carousel.init(carousel, {
-  //   fullWidth: true,
-  //   indicators: true,
-  //   autoScroll: 6000,
-  // });
-});
+(function ($) {
+  $(function () {
+    $(".scrollspy").scrollSpy();
+    $(".carousel").carousel({ padding: 100 });
+    $(".button-collapse").sideNav();
+    $(".collapsible").collapsible();
+    $("#modal1").modal();
+    $("#modal2").modal();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
 
-// Sidenav
-
-// (function($){
-//   $(function(){
-//     $('.scrollspy').scrollSpy();
-//     $('.carousel').carousel({padding: 100,});
-//     $('.button-collapse').sideNav();
-//     $('.collapsible').collapsible();
-//     $('.tap-target').tapTarget();
-//     $('#modal1').modal();
-//     $('#modal2').modal();
-
-//   }); // end of document ready
-// })(jQuery); // end of jQuery name space
-
-// // Autoplay
-// autoplay()
-// function autoplay() {
-//     $('.carousel').carousel('next');
-//     setTimeout(autoplay, 3500);
-// }
+// Autoplay
+autoplay();
+function autoplay() {
+  $(".carousel").carousel("next");
+  setTimeout(autoplay, 3500);
+}
